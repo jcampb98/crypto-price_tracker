d67dc74cbb98556c1f277173b7cd333205dfac69
jcampb98 <caboose768@gmail.com>
2022-05-26 12:49:04 +0100

    added the api data to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import Coin from './Coin';
 
 function App() {
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res =>{
       setCoins(res.data);
     }).catch(error => console.log('There is an Error in API...'))
   })
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filterCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <div className="coin-app">
@@ -18,11 +28,24 @@ function App() {
           Search Currency
         </h1>
         <form>
-          <input type="text" placeholder='Search Crypto' className='coin-input'/>
+          <input type="text" placeholder='Search Crypto' 
+          className='coin-input'onChange={handleChange}/>
         </form>
       </div>
-
-
+      {filterCoins.map(coin => {
+        return(
+          <Coin 
+            key={coin.id} 
+            name={coin.name} 
+            image={coin.image} 
+            symbol={coin.symbol} 
+            marketcap={coin.market_cap} 
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
   );
 }
